24d62dc8a53ba5e85e2d58e7be9a1000a53743f6
Ézsely Szovát <ezselyszovat@gmail.com>
2021-10-23 09:59:39 +0200

    treatments added

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { TreatmentComponent } from './treatment/treatment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    TreatmentComponent
   ],
   imports: [
     BrowserModule,
