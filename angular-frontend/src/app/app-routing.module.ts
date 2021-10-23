b9534699a2c82d8c065d438f19d01a96db4a2a45
Ézsely Szovát <ezselyszovat@gmail.com>
2021-10-23 16:40:46 +0200

    try on routing

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OccasionComponent } from './occasion/occasion.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserComponent},
+  { path: 'occasions', component: OccasionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
