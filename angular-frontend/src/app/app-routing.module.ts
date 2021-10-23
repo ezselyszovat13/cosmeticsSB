691037a92dd37a75353aaca2283781f9f94daef7
Ézsely Szovát <ezselyszovat@gmail.com>
2021-10-23 17:56:09 +0200

    routing between registration, login, resetpassword + header

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/components/login/login.component';
+import { RegisterComponent } from './auth/components/register/register.component';
+import { ResetPasswordComponent } from './auth/components/reset-password/reset-password.component';
 import { OccasionComponent } from './occasion/occasion.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-  { path: '', component: UserComponent},
-  { path: 'occasions', component: OccasionComponent}
+  { path: '', component: OccasionComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'reset-password', component: ResetPasswordComponent}
 ];
 
 @NgModule({
